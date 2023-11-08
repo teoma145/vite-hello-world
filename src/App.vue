3f7b84389d2507ffb2bb95e676e80dc7e8bbaeb0
teoma145 <matteodark145@yahoo.it>
2023-11-08 16:02:24 +0100

    creation-viteproject

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <h1>lol</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
